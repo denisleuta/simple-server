3f5df37c2010470372949e40aa27bec9c20f0fb9
denisleuta <146765040+denisleuta@users.noreply.github.com>
2024-06-05 00:07:22 +0300

    Update Game.cpp

--- Game.cpp
+++ Game.cpp
@@ -1,9 +1,9 @@
 #include "Game.h"
 
 using namespace std;
-
-bool game_running = true; // Глобальная переменная для управления циклом игры
-
+bool Game::game_running = true;
+int Game::score = 0;
+std::string Game::playerName;
 
 void Game::start_game() {
     setlocale(LC_ALL, "RU");
@@ -11,47 +11,47 @@ void Game::start_game() {
     double gameTime = 0.0;
     HighScoreManager highScoreManager;
 
-    while (game_running) { // Проверяем, запущена ли игра
+    while (game_running) { 
         SnakeGame game(20, 20);
         game.Setup();
         game.RunGameLoop();
 
         gameTime = gameTimer.GetElapsedTime();
-        cout << "Время вашей игровой сессии: " << gameTime << " секунд" << endl;
+        cout << "Р’СЂРµРјСЏ РІР°С€РµР№ РёРіСЂРѕРІРѕР№ СЃРµСЃСЃРёРё: " << gameTime << " СЃРµРєСѓРЅРґ" << endl;
 
-        int score = game.GetScore();
+        score = game.GetScore(); 
 
-        cout << "Введите ваше имя: ";
+        cout << "Р’РІРµРґРёС‚Рµ РІР°С€Рµ РёРјСЏ: ";
         string playerName;
         cin >> playerName;
 
         highScoreManager.SaveHighScore(playerName, score);
         highScoreManager.SaveHighScore(playerName, score);
 
-        // Добавляем запись результата в базу данных
-        std::string sql = "INSERT INTO results (name, result) VALUES ('" + playerName + "', " + std::to_string(score) + ")";
-        execute_sql_update(sql);
-        std::cout << "Результат игры успешно записан в базу данных." << std::endl;
-
-        cout << "Лучшие счета:\n";
+        cout << "Р›СѓС‡С€РёРµ СЃС‡РµС‚Р°:\n";
         vector<pair<string, int>> highScores = highScoreManager.GetHighScores();
         for (const auto& entry : highScores) {
             cout << entry.first << ": " << entry.second << endl;
         }
 
-        // Здесь можно добавить проверку для остановки игры по желанию игрока
-        cout << "Хотите продолжить игру? (yes/no): ";
+        send_results_to_server(playerName, score);
+
+        cout << "РҐРѕС‚РёС‚Рµ РїСЂРѕРґРѕР»Р¶РёС‚СЊ РёРіСЂСѓ? (yes/no): ";
         string continue_game;
         cin >> continue_game;
         if (continue_game == "no") {
-            stop_game(); // Вызываем функцию stop_game()
+            stop_game(); 
+            break; 
         }
 
-        this_thread::sleep_for(chrono::seconds(3));
+        this_thread::sleep_for(chrono::seconds(5));
     }
 }
 
-// Определение функции stop_game()
 void Game::stop_game() {
-    game_running = false; // Устанавливаем флаг остановки игры
+    game_running = false;
+}
+
+int Game::getScore() {
+    return score;
 }
